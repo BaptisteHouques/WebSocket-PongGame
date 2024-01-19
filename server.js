8559ee6f5e02050d730c369fc4ea9cb12acf56a7
BaptisteHouques <baptistehouques38@gmail.com>
2024-01-19 14:32:27 +0100

    add comments in server.js

--- server.js
+++ server.js
@@ -9,37 +9,37 @@ const io = new Server(server);
 
 app.use(express.static(join(__dirname, 'public')));
 
+// Page d'accueil
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'public/index.html'));
 });
 
+// Stockage des informations sur les rooms
 let rooms = {};
 
+// Gestionnaire de connexion pour les nouveaux clients WebSocket
 io.on('connection', (socket) => {
-    // Envoi immédiat la liste actuelle des rooms au nouveau client
+    // Envoie la liste actuelle des rooms à chaque nouveau client
     io.emit('updateRoomsList', Object.values(rooms).map(room => {
         return {
             id: room.id,
-            isFull: room.players.length >= 2
+            isFull: room.players.length >= 2 // Indique si la room est pleine
         };
     }));
 
+    // Gestion des événements de création et de jonction de room
     socket.on('createRoom', () => createRoom(socket));
     socket.on('joinRoom', (roomId) => joinRoom(socket, roomId));
 
-    // Ajout d'un gestionnaire d'événements pour mettre à jour la position de la balle
+    // Mise à jour de la position de la balle et transmission aux joueurs de la room
     socket.on('updateBallPosition', (data) => {
         const roomId = data.roomId;
         if (rooms[roomId]) {
-            // Mettre à jour la position de la balle à partir des données reçues
-            ballX = data.x;
-            ballY = data.y;
-
-            // Diffusez la nouvelle position de la balle à tous les joueurs
-            io.in(roomId).emit('ballMoved', { x: ballX, y: ballY });
+            io.in(roomId).emit('ballMoved', { x: data.x, y: data.y });
         }
     });
 
+    // Gestion du mouvement des raquettes et transmission aux joueurs de la room
     socket.on('movePaddle', (data) => {
         const roomId = data.roomId;
         if (rooms[roomId]) {
@@ -47,6 +47,7 @@ io.on('connection', (socket) => {
         }
     });
 
+    // Gestion de la déconnexion d'un client
     socket.on('disconnect', () => {
         for (const roomId in rooms) {
             const room = rooms[roomId];
@@ -63,6 +64,7 @@ io.on('connection', (socket) => {
         }
     });
 
+    // Mise à jour des scores et transmission aux joueurs de la room
     socket.on('updateScore', (data) => {
         const { scoreLeft, scoreRight, roomId } = data;
         if (rooms[roomId]) {
@@ -72,24 +74,26 @@ io.on('connection', (socket) => {
 
 });
 
+// Fonction pour créer une nouvelle room
 function createRoom(socket) {
     const roomId = 'room_' + Math.random().toString(36).substr(2, 5);
     rooms[roomId] = { id: roomId, players: [socket.id], isGameStarted: false };
     rooms[roomId].score = { left: 0, right: 0 };
     socket.join(roomId);
     socket.emit('roomCreated', roomId);
-    socket.emit('assignRole', 'left');
-    io.emit('updateRoomsList', Object.values(rooms));
+    socket.emit('assignRole', 'left'); // Assigner le rôle de joueur gauche au créateur de la room
+    io.emit('updateRoomsList', Object.values(rooms)); // Mettre à jour la liste des rooms
 }
 
+// Fonction pour rejoindre une room existante
 function joinRoom(socket, roomId) {
     if (rooms[roomId] && rooms[roomId].players.length < 2) {
         rooms[roomId].players.push(socket.id);
         socket.join(roomId);
         socket.emit('joinedRoom', roomId);
-        socket.emit('assignRole', 'right');
+        socket.emit('assignRole', 'right'); // Assigner le rôle de joueur droit au joueur qui rejoint
         if (rooms[roomId].players.length === 2) {
-            rooms[roomId].isGameStarted = true;
+            rooms[roomId].isGameStarted = true; // Commencer le jeu lorsque deux joueurs sont présents
             io.in(roomId).emit('startGame', roomId);
         }
     }
